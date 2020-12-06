e8ca668a5f084ad462988437e9f8676bb58ed05f
Areichao <71451192+Areichao@users.noreply.github.com>
2020-12-06 05:00:06 -0500

    Add files via upload

---
+++ Functions in program/Calculate score.py
@@ -0,0 +1,33 @@
+import pygame
+
+# Function for calculating score
+def calculatescore(playerScore,playerMatches):
+    # list of how may of each we have
+    playerTableCount = [0, 0, 0]
+
+    # For each of the different values, add one
+
+    for m in playerMatches:
+        if m == '25':
+            playerTableCount[0] += 1
+        elif m == '50':
+            playerTableCount[1] += 1
+        else:
+            playerTableCount[2] += 1
+
+    # Divide each one by two, since 2 cards only count as one value
+    newPlayerTableCount = []
+    for amount in playerTableCount:
+        amount = amount // 2
+        newPlayerTableCount.append(amount)
+
+    # Turn values into a score
+    playerScore = (((newPlayerTableCount[0])*25)+((newPlayerTableCount[1])*50)+((newPlayerTableCount[2])*100))
+
+# Calling the function
+# If player 1's turn
+if (turn.p_turn == 1):
+    calculatescore(player1Score,player1Matches)
+# If player 2's turn
+else:
+    calculatescore(player2Score,player2Matches)
---
+++ Functions in program/viewTable.py
@@ -0,0 +1,32 @@
+from pygame import *
+
+# their low 547, 498
+# their med 1003, 496
+# their high 1458, 499
+# your low 544, 873
+# your med 1000, 888
+# your high 1459, 886
+tableCardCountLocations = [[(544, 873),(1000, 888),(1459, 886)],[(547, 498),(1003, 496),(1458, 499)]]
+
+def viewTable(yourMatch,opponentMatch):
+    #background
+    screen.blit(pygame.image.load('./images/table' + f'{turn.p_turn}' + '.png'), (0,0))
+
+    viewDefault()
+    numberMatches()
+
+    # Put current players stuff on screen
+    for i in range(len(yourMatch)):
+        GAME_FONT.render_to(screen,tableCardCountLocations[0][i], f'{yourMatch[i]}', (104, 202, 244))
+    # Put opponent's stuff on screen
+    for i in range(len(opponentMatch)):
+        GAME_FONT.render_to(screen, tableCardCountLocations[1][i], f'{opponentMatch[i]}', (104, 202, 244))
+
+# Calling function
+# if it is player 1's turn
+if (turn.p_turn == 1):
+    viewTable(p1_tablecounts,p2_tablecounts)
+# if it is player 2's turn
+else:
+    viewTable(p2_tablecounts,p1_tablecounts)
+

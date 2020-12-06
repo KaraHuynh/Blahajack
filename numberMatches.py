628499b78ba904ce0062194b5435599da969b657
Areichao <71451192+Areichao@users.noreply.github.com>
2020-12-06 03:30:41 -0500

    Update numberMatches.py

--- numberMatches.py
+++ numberMatches.py
@@ -1,53 +1,33 @@
 import pygame
 
-
-def number_Matches(player1Matches,player2Matches):
+def numberMatches(player1Matches,player2Matches):
 
     # For each of the different values, add one
-
-    for i, m in enumerate(player1Matches):
+    p1_match = [0,0,0]
+    for m in player1Matches:
         if m == '25':
-            p1_tablecounts[0] += 1
+            p1_match[0] += 1
         elif m == '50':
-            p1_tablecounts[1] += 1
+            p1_match[1] += 1
         else:
-            p1_tablecounts[2] += 1
+            p1_match[2] += 1
 
-            # Divide each one by two, since 2 cards only count as one value
-
-    for amount in p1_tablecounts:
-        amount = amount // 2
+    # Divide each one by two, since 2 cards only count as one value
+    for amount in range(len(p1_match)):
+        p1_match[amount] = (p1_match[amount]) // 2
+        p1_tablecounts[amount] += p1_match[amount]
 
     # Repeat for opposite player
-
-    for i, m in enumerate(player2Matches):
+    p2_match = [0,0,0]
+    for m in player2Matches:
         if m == '25':
-            p2_tablecounts[0] += 1
+            p2_match[0] += 1
         elif m == '50':
-            p2_tablecounts[1] += 1
+            p2_match[1] += 1
         else:
-            p2_tablecounts[2] += 1
+            p2_match[2] += 1
 
-    for amount in p2_tablecounts:
-        amount = amount // 2
-
-def viewTable():
-    #background
-    screen.blit(pygame.image.load('./images/table' + f'{turn.p_turn}' + '.png'), (0,0))
-
-    number_Matches()
-    viewDefault()
-
-    # Put player 1's stuff onto screen
-    print(f'p1cPRINTING:{player1Counts}')
-    for i in range(len(p1_tablecounts))
-
-    #their low 547, 498
-    #their med 1003, 496
-    #their high 1458, 499
-    #your low 544, 873
-    #your med 1000, 888
-    #your high 1459, 886
-
-
+    for amount in range(len(p2_match)):
+        p2_match[amount] = (p2_match[amount]) // 2
+        p2_tablecounts[amount] += p2_match[amount]
 
